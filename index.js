6a5f1dc6d4e62c600928069677b4e930da4a1f95
max-hagi <msavehilaghi99@gmail.com>
2022-11-14 14:25:04 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+let countElement = document.getElementById("count")
+let count = 0
+
+function increment() {
+    count = count + 1
+    countElement.innerText = count
+    console.log(count)
+}
+
+function save(){
+
+}
